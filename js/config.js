6fb429c4aa68212c8d827a48ed5ba0e06f6eae17
mauriciojolivac1 <mauricio.oliva@gmail.com>
2026-03-23 22:04:56 -0300

    Configurar URL de backend en Render

--- js/config.js
+++ js/config.js
@@ -3,4 +3,4 @@
  * Vacío = mismo origen (p. ej. detrás de un proxy que une front y API).
  * Desarrollo local con dos puertos: "http://localhost:3001"
  */
-export const API_BASE = "http://localhost:3001";
+export const API_BASE = "https://backend-7wy9.onrender.com";
